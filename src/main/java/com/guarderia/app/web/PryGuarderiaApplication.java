8ce2c3a3f47eb5841bf93c31e02c59d722636f5c
Luis Pillaga <luisp-98@live.com>
2019-04-30 20:18:04 -0500

    Initial commit

---
+++ src/main/java/com/guarderia/app/web/PryGuarderiaApplication.java
@@ -0,0 +1,13 @@
+package com.guarderia.app.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PryGuarderiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PryGuarderiaApplication.class, args);
+    }
+
+}
